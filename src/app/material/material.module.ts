bb69660e02154384c0d22151f0098ac2e3128878
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2021-08-22 17:51:56 +0300

    Material demo: buttons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
